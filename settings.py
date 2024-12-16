18fe66c939b03366b1eb07e04d861dca959f2205
AveryMerlo-Pace <avery.merlo@paceacademy.org>
2024-12-16 12:32:24 -0500

    1st Iteration

---
+++ settings.py
@@ -0,0 +1,20 @@
+class Settings:
+    
+    def __init__(self):
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+        
+        self.ship_speed = 3.0
+        self.ship_limit = 3
+        
+        self.bullet_speed = 5.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 10
+        
+        self.alien_speed = 2.5 
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+        
